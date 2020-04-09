c16f86af00c7bd1a2a8a91ba71663bdf0e734f09
Targhib49 <targhib.ibrahim@gmail.com>
2020-04-09 09:17:24 +0700

    new function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,77 +1,88 @@
 //Elements 
-let calcButton = document.getElementById("calc-button")
-let lowerDisplay = document.getElementById("lower-display")
-lowerDisplay.innerText = "0";
+let basicButton = document.getElementById("basic-button")
+let pwidButton = document.getElementById("pwid-button")
+let fsButton = document.getElementById("fs-button")
 
-let count = 0;
+let basicInput1 = document.getElementById("basic-input-1")
+let basicInput2 = document.getElementById("basic-input-2")
+let pwidInput1 = document.getElementById("pwid-input-1")
+let pwidInput2 = document.getElementById("pwid-input-2")
+let fsInput1 = document.getElementById("fs-input")
 
-// let quotesGeneratorScreen = document.getElementById("quotes-screen")
-// let quotesGeneratorButton = document.getElementById("quotes-button")
+let basicResult = document.getElementById("basic-result")
+let pwidResult = document.getElementById("pwid-result")
+let fsResult = document.getElementById("fs-result")
 
-/*
-const operation = (op1, oper, op2) => {
-  
-}*/
+let basicEqual = document.getElementById("basic-equal")
+let pwidEqual = document.getElementById("pwid-equal")
+let fsEqual = document.getElementById("fs-equal")
 
-const display = event => {
-    if (count < 18) {
-        if (event.target.matches(".numButton")) {
-            let number = event.target.id.replace("no-", "")
-            if (lowerDisplay.innerText == 0) {
-                lowerDisplay.innerText = number
-            } else {
-                lowerDisplay.innerText += number
-            }
-        }
+let quotesGeneratorScreen = document.getElementById("quotes-screen")
+let quotesGeneratorButton = document.getElementById("quotes-button")
 
-        if (event.target.matches(".opButton")) {
-            let oper = ""
-            let operInd = event.target.id.replace("op-", "")
-            if (operInd == 1) {
-                oper = "+"
-            } else if (operInd == 2) {
-                oper = "-"
-            } else if (operInd == 3) {
-                oper = "x"
-            } else if (operInd == 4) {
-                oper = "/"
-            } else if (operInd == 5) {
-                oper = "^"
-            } else if (operInd == 6) {
-                oper = "%"
-            } else if (operInd == 7) {
-                oper = "\\"
-            } else if (operInd == 8) {
-                oper = "!"
-            }
-            lowerDisplay.innerText += oper
-        }
+let oper
+let readBasic1 = 0
+let readBasic2 = 0
+let readPWID1 = 0
+let readPWID2 = 0
+let readFS = 0
+let count = 1
 
-        if (event.target.matches(".myButton")) {
-            let button = ""
-            let buttonInd = event.target.id.replace("but-", "")
-            if (buttonInd == 1) {
-                alert("This button is under maintenance")
-            } else if (buttonInd == 2) {
-                button = "."
-            } else if (buttonInd == 3) {
-                alert("in")
-            } else if (buttonInd == 4) {
-                lowerDisplay.innerText = 0
-            } else if (buttonInd == 5) {
-                alert("This button is under maintenance")
-            } else if (buttonInd == 6) {
-                alert("This button is under maintenance")
-            }
-            lowerDisplay.innerText += button
-        }
 
-        count++
-    } else {
-        alert("Max number limit reached")
+const operatorSelection = event => {
+    if (event.target.matches(".btn-basic")){
+        oper = event.target.id.replace("op-","")
     }
+
+    if (event.target.matches(".btn-pwid")){
+        oper = event.target.id.replace("op-","")
+    }
+
+    if (event.target.matches(".btn-fs")){
+        oper = event.target.id.replace("op-","")
+    }
+    console.log(oper);
+    
 }
-/*
+
+const readInput = event => {
+    readBasic1 = Number(basicInput1.value)
+    readBasic2 = Number(basicInput2.value)
+    readPWID1 = Number(pwidInput1.value)
+    readPWID2 = Number(pwidInput2.value)
+    readFS = Number(fsInput1.value)
+}
+
+const operation = event => {
+    let resultBasic = 0
+    let resultPWID = 0
+    let resultFS = 0
+
+    if (oper == "+"){
+        resultBasic = readBasic1 + readBasic2
+    } else if (oper == "-"){
+        resultBasic = readBasic1 - readBasic2
+    } else if (oper == "X"){
+        resultBasic = readBasic1 * readBasic2
+    } else if (oper == "/"){
+        resultBasic = readBasic1 / readBasic2
+    } else if (oper == "^"){
+        resultPWID = readPWID1 ** readPWID2
+    } else if (oper == "%"){
+        resultPWID = readPWID1 % readPWID2
+    } else if (oper == "\\"){
+        alert("Under maintenance")
+    } else if (oper == "!"){
+        alert("Under maintenance")
+    } else if (oper == "sqrt"){
+        resultFS = Math.sqrt(readFS)
+    }
+    
+    basicResult.innerText = resultBasic
+    pwidResult.innerText = resultPWID
+    fsResult.innerText = resultFS
+}   
+
 const quotesGen = () => {
     let quotes1 = "“Mathematics expresses values that reflect the cosmos, including orderliness, balance, harmony, logic, and abstract beauty.”<br> ― Deepak Chopra "
     let quotes2 = "“The difference between the poet and the mathematician is that the poet tries to get his head into the heavens while the mathematician tries to get the heavens into his head.”<br>― G.K. Chesterton"
@@ -97,10 +108,22 @@ const quotesGen = () => {
         quotesGeneratorScreen.innerHTML = quotes5
     }
 
-}*/
+}
 
+readInput()
 
-calcButton.addEventListener("click", display)
-// operatorButton.addEventListener("click", display)
-// secondOperandButton.addEventListener("click", display)
-// quotesGeneratorButton.addEventListener("click", quotesGen)
+basicButton.addEventListener("click", operatorSelection)
+pwidButton.addEventListener("click", operatorSelection)
+fsButton.addEventListener("click", operatorSelection)
+
+basicInput1.addEventListener("keyup", readInput)
+basicInput2.addEventListener("keyup", readInput)
+pwidInput1.addEventListener("keyup", readInput)
+pwidInput2.addEventListener("keyup", readInput)
+fsInput1.addEventListener("keyup", readInput)
+
+basicEqual.addEventListener("click", operation)
+pwidEqual.addEventListener("click", operation)
+fsEqual.addEventListener("click", operation)
+
+quotesGeneratorButton.addEventListener("click", quotesGen)
